2a244566e28ef2fe8641310616b42eed32092d3d
val.gee <vguevara0806@gmail.com>
2023-01-10 20:03:45 -0500

    defined columns for each model and synced sequelize models to the database

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -16,6 +17,32 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      },
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
